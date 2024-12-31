e7439826d4e72ce070790351673845be2443a9ea
jdh8643 <jdh8643@naver.com>
2024-12-31 15:52:56 +0900

    feat: Add route setup and basic components structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"
+
+
+const Layout = () => {
+  return (
+    <div>
+    <header>header</header>
+    <Outlet />
+
+    </div>
+  )
+}
+
+export default Layout
