32cfff1d9e6afa22db7d7dac4d598aa57e1fb7f3
jdh8643 <jdh8643@naver.com>
2025-01-02 12:46:38 +0900

    네비

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "<주소>";
+const supabaseUrl = "https://doktkgirvqvjtybkhnnp.supabase.co";
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
