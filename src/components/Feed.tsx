d52feb825f2a6e1166d7296820f76615d125cb24
jdh8643 <jdh8643@naver.com>
2025-01-13 18:50:39 +0900

    map뿌리기

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import { FaAngleUp, FaCommentDots } from "react-icons/fa6";
 
-const Feed = () => {
+
+interface feedProps{
+  id:string;
+  title:string
+  content:string
+  created_at:string
+  user_id:string
+}
+
+const Feed = ({feed}:{feed:feedProps}) => {
   return (
     <Link
       to="/detail/1"
@@ -15,10 +24,10 @@ const Feed = () => {
       </div>
       <div className="flex-1 px-10 min-w-0 flex flex-col gap-4">
         <div className="flex flex-col gap-2">
-          <h2 className="text-blue-950 text-xl font-bold">첫 번째 게시물</h2>
-          <p className="text-gray-600 truncate text-md">날씨가 맑다!!!!크아!!!!!</p>
+          <h2 className="text-blue-950 text-xl font-bold">{feed.title}</h2>
+          <p className="text-gray-600 truncate text-md">{feed.content}</p>
         </div>
-        <p className="text-gray-600 text-xs text-right">"작성일:" "2025.01.01"</p>
+        <p className="text-gray-600 text-xs text-right">"작성일:" {new Date(feed.created_at).toLocaleDateString()}</p>
       </div>
       <div className="flex items-center gap-1 p-3 text-gray-600">
       <FaCommentDots className="text-gray-500 font-bold text-xl" />
