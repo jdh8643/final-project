4038dfae0a6e2aa6e2b0b4004b6e088e8fba9b6c
jdh8643 <jdh8643@naver.com>
2025-01-14 18:23:34 +0900

    feed의 좋아요,댓글개수

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import { FaAngleUp, FaCommentDots } from "react-icons/fa6";
+import { useQuery } from "@tanstack/react-query";
+import { getUpvotesCount } from "../api/upvoteApi";
+import { getCommentsCount } from "../api/commentApi";
 
 
 interface feedProps{
@@ -11,6 +14,16 @@ interface feedProps{
 }
 
 const Feed = ({feed}:{feed:feedProps}) => {
+
+const {data:commentsCount, isLoading:isCommentsLoading} = useQuery({
+  queryKey:['comments', feed.id],
+  queryFn:()=> getCommentsCount(feed.id)
+});
+const {data:upvotesCount, isLoading:isUpvotesLoading} = useQuery({
+  queryKey:['upvotes', feed.id],
+  queryFn:()=> getUpvotesCount(feed.id)
+});
+
   return (
     <Link
       to="/detail/1"
@@ -19,7 +32,7 @@ const Feed = ({feed}:{feed:feedProps}) => {
       <div>
         <button className="p-3 bg-gray-100 rounded-lg text-sm flex-col items-center gap-1 text-bulue-950">
           <FaAngleUp className="text-xs text-center font-bold" />
-          <div className="font-bold">1</div>
+          <div className="font-bold">{isUpvotesLoading ? '...' : upvotesCount}</div>
         </button>
       </div>
       <div className="flex-1 px-10 min-w-0 flex flex-col gap-4">
@@ -31,7 +44,7 @@ const Feed = ({feed}:{feed:feedProps}) => {
       </div>
       <div className="flex items-center gap-1 p-3 text-gray-600">
       <FaCommentDots className="text-gray-500 font-bold text-xl" />
-        <div className="font-bold">4</div>
+        <div className="font-bold">{isCommentsLoading ? '...' : commentsCount}</div>
       </div>
     </Link>
   );
