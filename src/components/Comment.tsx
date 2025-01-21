32becaf42b637ce038274b9c1c78e81d68535fd2
jdh8643 <jdh8643@naver.com>
2025-01-21 20:12:11 +0900

    너무 밀림

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,45 +1,54 @@
 import { IoPersonCircleOutline } from "react-icons/io5";
+import { useAuthStore } from "../stores/useAuthStore";
 
 interface CommentProps {
-    id: string;
-    content: string;
-    created_at: string;
-    feed_id: string;
-    user_id: string;
-    user: {
-        id: string;
-        nickname: string;
-        email: string;
-        img_url: string;
-    }
+	id: string;
+	content: string;
+	created_at: string;
+	feed_id: string;
+	user_id: string;
+	user: {
+		id: string;
+		nickname: string;
+		email: string;
+		img_url: string;
+	}
 }
 
-const Comment = ({ comment }: { comment: CommentProps }) => {
-  return (
-    <>
-    <div className="flex gap-2.5">
-        {comment.user.img_url ?
-         <img src={comment.user.img_url} alt="profile" className="w-12 h-12 rounded-full mr-6"/> : 
-         <IoPersonCircleOutline className="w-12 h-12 rounded-full mr-6"/>}
-        
-        <div className="flex flex-col flex-1 gap-3">
-            <div className="flex flex-col gap-1">
-                <div className="text-slate-900 font-bold text-sm">
-                    {comment.user.nickname? comment.user.nickname : comment.user.email}
-                </div>
-            </div>
-            <div className="text-gray-500">
-                {comment.content}
-            </div>
-        </div>
-        <div className="flex items-end gap-2">
-            <button className="text-white bg-blue-500 px-4 py-2 rounded-md">수정</button>
-            <button className="text-white bg-red-500 px-4 py-2 rounded-md">삭제</button>
-        </div>
-    </div>
-    <hr className="m-0 border-t border-gray-200"/>
-    </>
-  )
-}
+export default function Comment({ comment }: { comment: CommentProps }) {
+	const { user } = useAuthStore();
 
-export default Comment
+	return (
+		<>
+			<div className="flex gap-2.5">
+				{comment.user.img_url ?
+					<img src={comment.user.img_url} alt="profile" className="w-12 h-12 rounded-full mr-6" /> :
+					<IoPersonCircleOutline className="w-12 h-12 rounded-full mr-6" />}
+				<div className="flex flex-1 flex-col gap-3">
+					<div className="flex flex-col gap-1">
+						<div className="text-slate-900 font-bold text-sm">
+							{comment.user.nickname ? comment.user.nickname : comment.user.email}
+						</div>
+					</div>
+					<div className="text-gray-500">{comment.content}</div>
+				</div>
+				{/* 내가 로그인한 경우에만 보인다. */}
+				{user?.id === comment.user_id ?
+					(<div className="flex items-end gap-2">
+						<button
+							className="text-white bg-yellow-500 px-4 py-2 rounded-md"
+						>
+							수정
+						</button>
+						<button
+							className="text-white bg-red-500 px-4 py-2 rounded-md"
+						>
+							삭제
+						</button>
+					</div>
+					) : null}
+			</div>
+			<hr className="m-0 border-t border-gray-200" />
+		</>
+	)
+}
--- src/providers/AuthProvider.ts
+++ src/providers/AuthProvider.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import supabase from "../utils/supabase";
-import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../stores/useAuthStore";
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const setUser = useAuthStore((state) => state.setUser);
